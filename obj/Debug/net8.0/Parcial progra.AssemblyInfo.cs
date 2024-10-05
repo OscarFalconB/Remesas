47832b5804c4c863b4e9dd7af956feb64d3bd183
Oscar Falcon <sebasfalcon23@gmail.com>
2024-10-05 06:15:21 -0500

    Merge

--- obj/Debug/net8.0/Parcial progra.AssemblyInfo.cs
+++ obj/Debug/net8.0/Parcial progra.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Parcial progra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3eed3e22d35c471a7ba613240651af401654a33c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Parcial progra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Parcial progra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
